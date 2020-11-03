be1368d744b4fc16ef20c7dbcf8635f2bc14f2a2
Dinxor <61650836+Dinxor@users.noreply.github.com>
2020-11-03 22:44:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -118,5 +118,5 @@ def upload_file():
     '''
 
 if __name__ == '__main__':
-    http_server = WSGIServer(('0.0.0.0', 5000), app)
-    http_server.serve_forever()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
